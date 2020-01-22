aaaba97f3b3b6a347d9a53b57542acdc6f819079
Danny Tan <danny@dannys-mbp.home>
2020-01-22 22:16:27 +0100

    i dont get it

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace microservices_project {
         app.Run(async (context) =>
         {
           await context.Response.WriteAsync("Hello, world!");
-          await context.Response.WriteAsync("Testing whether Wercker creates image on Docker hub");
+          await context.Response.WriteAsync("Desperation ! ! ");
         });
       }
   }
